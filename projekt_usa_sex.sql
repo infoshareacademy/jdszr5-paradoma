564ef688357de98f5d00379cd54df8b98d351165
Paulina <paulina.w.rakowska@gmail.com>
2021-10-08 18:02:03 +0200

    Update projekt_usa_sex.sql

--- projekt_usa_sex.sql
+++ projekt_usa_sex.sql
@@ -1,4 +1,4 @@
--- g³osowanie w zale¿noœci od p³ci
+-- gÂ³osowanie w zaleÂ¿noÅ“ci od pÂ³ci
 create table sex as
 select 
 	cf.fips,
@@ -12,14 +12,14 @@ select
 from county_facts cf
 join primary_results pr on cf.fips = pr.fips 
 
-with table_wic as -- obliczenie przedzia³ów
+with table_wic as -- obliczenie przedziaÂ³Ã³w
 (select 
 	max(s.women_in_county) as max_wic,
 	min(s.women_in_county),
 	(max(s.women_in_county) - min(s.women_in_county)) / 4 as interval_wic
-from sex s )
-
-select -- obliczenie granicy przedzia³u z najbardziej liczn¹ grup¹
+from sex s 
+)
+select -- obliczenie granicy przedziaÂ³u z najbardziej licznÂ¹ grupÂ¹
 	(max_wic - interval_wic)
 from table_wic
 
@@ -32,7 +32,7 @@ from sex s
 where s.women_in_county between 50.1 and 57.0
 order by sum_votes_candidate DESC
 
-select -- w których stanach jest najwiêcej kobiet 
+select -- w ktÃ³rych stanach jest najwiÃªcej kobiet 
 	cf.area_name,
 	cf."SEX255214" 
 from county_facts cf 
@@ -40,7 +40,7 @@ where cf.fips like '%000' and cf."SEX255214" > 51.0
 order by cf."SEX255214" desc
 
 
---kolejne przedzia³y, co siê dzieje kiedy spada liczba kobiet
+--kolejne przedziaÂ³y, co siÃª dzieje kiedy spada liczba kobiet
 
 SELECT 
 	distinct(s.candidate),
@@ -69,7 +69,7 @@ from sex s
 where s.women_in_county between 30.0 and 36.7 
 order by sum_votes_candidate DESC
 
-select -- w których stanach jest najwiêcej mêzczyzn 
+select -- w ktÃ³rych stanach jest najwiÃªcej mÃªzczyzn 
 	cf.area_name,
 	cf."SEX255214" 
 from county_facts cf 
